c85d9d792bd83091b3399d125515cf813bfd144e
Yasmin Lopes <yclopes27@gmail.com>
2023-03-14 22:30:25 -0300

    test: add tests for button component

--- src/app/global/button/button.component.spec.ts
+++ src/app/global/button/button.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ButtonComponent } from './button.component';
 
 describe('ButtonComponent', () => {
@@ -8,16 +7,70 @@ describe('ButtonComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ButtonComponent ]
-    })
-    .compileComponents();
+      declarations: [ButtonComponent],
+    }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(ButtonComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('deve criar o componente', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('deve exibir o texto', () => {
+    component.text = 'Texto';
+    fixture.detectChanges();
+    const button = fixture.nativeElement.querySelector('.button');
+    expect(button.textContent).toContain('Texto');
+  });
+
+  it('deve exibir o ícone', () => {
+    component.icon = 'favorite';
+    fixture.detectChanges();
+    const button = fixture.nativeElement.querySelector('.button');
+    expect(button.querySelector('span').textContent).toContain('favorite');
+  });
+
+  it('deve ter a classe "outlined"', () => {
+    component.outlined = true;
+    fixture.detectChanges();
+    const button = fixture.nativeElement.querySelector('.button');
+    expect(button.classList).toContain('outlined');
+  });
+
+  it('deve ter a classe de cor', () => {
+    component.color = 'primary';
+    fixture.detectChanges();
+    const button = fixture.nativeElement.querySelector('.button');
+    expect(button.classList).toContain('primary');
+  });
+
+  it('deve ter a classe de cor de fundo', () => {
+    component.bgColor = 'primary';
+    fixture.detectChanges();
+    const button = fixture.nativeElement.querySelector('.button');
+    expect(button.classList).toContain('bg-primary');
+  });
+
+  it('deve emitir o evento onClick', () => {
+    spyOn(component.onClick, 'emit');
+    const button = fixture.nativeElement.querySelector('.button');
+    button.click();
+    expect(component.onClick.emit).toHaveBeenCalled();
+  });
+
+  it('deve atualizar a classe do ícone no evento mouseleave', () => {
+    component.icon = 'add';
+    fixture.detectChanges();
+    const button = fixture.nativeElement.querySelector('.button');
+    const icon = button.querySelector('span');
+    button.dispatchEvent(new Event('mouseleave'));
+    fixture.detectChanges();
+    expect(icon.classList).toContain('material-icons-outlined');
+    expect(icon.classList).not.toContain('add');
   });
 });
