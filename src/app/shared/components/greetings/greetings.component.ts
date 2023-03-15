b27230c3b8a8ed4ab67260c623e356fa83974f36
Yasmin Lopes <yclopes27@gmail.com>
2023-03-15 20:18:14 -0300

    feat: create greetings component

--- src/app/shared/components/greetings/greetings.component.ts
+++ src/app/shared/components/greetings/greetings.component.ts
@@ -1,4 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { DAYS_OF_WEEK, MONTHS } from './model/greetings';
+
+@Component({
+  selector: 'app-greetings',
+  templateUrl: './greetings.component.html',
+  styleUrls: ['./greetings.component.scss'],
+})
+export class GreetingsComponent implements OnInit {
+
   public currentDate: Date = new Date();
+  public daysOfWeek = DAYS_OF_WEEK;
+  public months = MONTHS;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
   getGreetings() {
     const currentHour = this.currentDate.getHours();
     return currentHour >= 6 && currentHour < 12
@@ -16,3 +33,4 @@
 
     return `${dayOfWeek}, ${month} ${dayOfMonth}, ${year}`;
   }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NavBarModule } from './components/navbar/navbar.module';
+import { GreetingsComponent } from './components/greetings/greetings.component';
 
 
 @NgModule({
-  declarations: [  
+  declarations: [    
+    GreetingsComponent
   ],
   imports: [
     BrowserModule,
     NavBarModule
   ],
-  exports: [NavBarModule],
+  exports: [
+    NavBarModule,
+    GreetingsComponent],
 })
 export class SharedModule {}
