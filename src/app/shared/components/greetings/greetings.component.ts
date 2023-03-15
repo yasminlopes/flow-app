0d64623462404f11e243cca3f67d97afc2a4475a
Yasmin Lopes <yclopes27@gmail.com>
2023-03-15 20:15:56 -0300

    feat: add function that returns appropriate greeting based on time of day

---
+++ src/app/shared/components/greetings/greetings.component.ts
@@ -0,0 +1,8 @@
+  getGreetings() {
+    const currentHour = this.currentDate.getHours();
+    return currentHour >= 6 && currentHour < 12
+      ? 'Bom dia!'
+      : currentHour >= 12 && currentHour < 18
+      ? 'Boa tarde!'
+      : 'Boa noite!'
+  }
