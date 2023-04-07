4684171631fc7c519859d4311040359165dfbcaf
Yasmin Lopes <yclopes27@gmail.com>
2023-04-07 18:58:34 -0300

    feat: implement card totalizer in home

--- src/app/features/home/container/home/home.component.ts
+++ src/app/features/home/container/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CARD_ITEMS } from '../../model/home-cards';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+
+  public cardItems = CARD_ITEMS
 
   constructor() { }
 
---
+++ src/app/features/home/model/home-cards.ts
@@ -0,0 +1,5 @@
+export const CARD_ITEMS = [
+    { icon: 'attach_money', title: 'Entrada', value: 3000 },
+    { icon: 'currency_exchange', title: 'Saída', value: 2500 },
+    { icon: 'account_balance', title: 'Total', value: 500 },
+];
