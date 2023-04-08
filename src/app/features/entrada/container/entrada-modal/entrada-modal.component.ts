f7026cda4e5301c78d10e58a8bc2eac9d0906f15
Yasmin Lopes <yclopes27@gmail.com>
2023-04-08 17:08:41 -0300

    feat: create modal for category form

--- src/app/features/entrada/container/entrada-modal/entrada-modal.component.ts
+++ src/app/features/entrada/container/entrada-modal/entrada-modal.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ModalComponent } from 'src/app/global/modal/modal.component';
 import { ModalService } from 'src/app/shared/services/modal.service';
+import { CATEGORY_ICONS_OPTIONS } from '../../models/entrada-const';
 
 @Component({
   selector: 'app-entrada-modal',
@@ -10,13 +12,34 @@ import { ModalService } from 'src/app/shared/services/modal.service';
 })
 export class EntradaModalComponent extends ModalComponent implements OnInit {
 
-  constructor( private modalService: ModalService) { super() }
+  public entradaCategoryForm: FormGroup;
+  public categoryOptions = CATEGORY_ICONS_OPTIONS;
+
+  constructor( 
+    private modalService: ModalService,
+    private formBuilder: FormBuilder
+  ) { 
+    super(); 
+  }
 
   override ngOnInit(): void {
+    this.initForm();
+    this.entradaCategoryForm.valueChanges.subscribe(x => console.log(x));
   }
 
-  fecharModal(){
-    this.modalService.closeModal()
+  public closeModal(){
+    this.modalService.closeModal();
   }
 
+  private initForm(){
+    this.entradaCategoryForm = this.formBuilder.group({
+      id: [''],
+      icon: ['', [Validators.required]],
+      name_category: ['', [Validators.required]],
+    });
+  }
+
+  public save(){
+    // implementar função
+  }
 }
