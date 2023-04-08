93b279ac56dfb91b3ae3678025541e8d3830c4cc
Yasmin Lopes <yclopes27@gmail.com>
2023-04-08 17:08:20 -0300

    feat: add category icons options list

---
+++ src/app/features/entrada/models/entrada-const.ts
@@ -0,0 +1,13 @@
+export const CATEGORY_ICONS_OPTIONS = [
+  { description: 'attach_money', value: 'attach_money' },
+  { description: 'show_chart', value: 'show_chart' },
+  { description: 'payment', value: 'payment' },
+  { description: 'pix', value: 'pix' },
+  { description: 'sell', value: 'sell' },
+  { description: 'design_services', value: 'design_services' },
+  { description: 'bolt', value: 'bolt' },
+  { description: 'moped', value: 'moped' },
+  { description: 'diversity_3', value: 'diversity_3' },
+  { description: 'maps_home_work', value: 'maps_home_work' },
+  { description: 'router', value: 'router' },
+];
