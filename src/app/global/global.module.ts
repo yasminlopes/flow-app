29c91ec5c77b72f88b73d869c0add92d58d48481
Yasmin Lopes <yclopes27@gmail.com>
2023-04-08 00:00:46 -0300

    feat: create input component

--- src/app/global/global.module.ts
+++ src/app/global/global.module.ts
@@ -1,23 +1,31 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; // importando o ReactiveFormsModule
 import { ButtonComponent } from './button/button.component';
 import { ContentComponent } from './content/content.component';
 import { ModalComponent } from './modal/modal.component';
 import { CardTotalizadorComponent } from './card/card-totalizador.component';
+import { InputComponent } from './input/input.component';
 
 @NgModule({
   declarations: [
     ButtonComponent,
     ContentComponent,
     ModalComponent,
-    CardTotalizadorComponent
+    CardTotalizadorComponent,
+    InputComponent
   ],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   exports: [
     ButtonComponent,
     ContentComponent,
     ModalComponent,
-    CardTotalizadorComponent
+    CardTotalizadorComponent,
+    InputComponent,
   ],
 })
 export class GlobalModule {}
---
+++ src/app/global/input/input.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit, Input, forwardRef} from '@angular/core';
+import { FormControl, FormGroup, NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true,
+    },
+  ],
+})
+export class InputComponent implements OnInit, ControlValueAccessor {
+
+  @Input() formControl: FormControl;
+  @Input() formGroup: FormGroup;
+  @Input() id: string;
+  @Input() label: string;
+  @Input() icon: string;
+  @Input() type: string;
+  @Input() formControlName: string;
+  @Input() iconLeft: string;
+  @Input() iconRight: string;
+  @Input() placeholder = '';
+  @Input() required: boolean = false;
+  @Input() disabled: boolean = false;
+  
+  public value: any = '';
+
+  onChange: any = () => {};
+  onTouch: any = () => {};
+
+  constructor() { }
+
+  ngOnInit(): void {}
+
+  writeValue(value: any): void {
+    this.value = value;
+    this.onChange(value);
+    this.onTouch();
+  }
+  
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  
+  registerOnTouched(fn: any): void {
+    this.onTouch = fn;
+  }
+  
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+  
+}
