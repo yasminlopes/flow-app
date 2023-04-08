3d32b7d0798789fff04e5627cdd2bf6e62222ec4
Yasmin Lopes <yclopes27@gmail.com>
2023-04-07 21:40:17 -0300

    feat: inherit class from modal

--- src/app/features/entrada/container/entrada-modal/entrada-modal.component.ts
+++ src/app/features/entrada/container/entrada-modal/entrada-modal.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ModalComponent } from 'src/app/global/modal/modal.component';
 import { ModalService } from 'src/app/shared/services/modal.service';
 
 @Component({
   selector: 'app-entrada-modal',
   templateUrl: './entrada-modal.component.html',
-  styleUrls: ['./entrada-modal.component.scss']
+  styleUrls: ['./entrada-modal.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class EntradaModalComponent implements OnInit {
+export class EntradaModalComponent extends ModalComponent implements OnInit {
 
-  constructor( private modalService: ModalService) { }
+  constructor( private modalService: ModalService) { super() }
 
-  ngOnInit(): void {
+  override ngOnInit(): void {
   }
 
   fecharModal(){
