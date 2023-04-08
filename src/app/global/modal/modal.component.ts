bc32f44a0b180def8858de9a5603a22c0ff660e5
Yasmin Lopes <yclopes27@gmail.com>
2023-04-07 21:37:49 -0300

    perf: improve modal component

--- src/app/global/modal/modal.component.ts
+++ src/app/global/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output, OnInit, Input } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
@@ -8,12 +8,12 @@ import { Component, EventEmitter, Output, OnInit, Input } from '@angular/core';
 
 export class ModalComponent implements OnInit {
   @Input() title: string;
+  @Input() saveText: string;
 
   @Output() onSave: EventEmitter<any> = new EventEmitter<any>();
   @Output() onClose: EventEmitter<any> = new EventEmitter<any>();
 
   constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 }
