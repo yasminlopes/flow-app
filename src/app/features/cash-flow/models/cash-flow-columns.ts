64daae9dd68795f43916924f47b924568e6e071b
Yasmin Lopes <yclopes27@gmail.com>
2023-04-23 16:32:31 -0300

    feat: add columns fields type

---
+++ src/app/features/cash-flow/models/cash-flow-columns.ts
@@ -0,0 +1,12 @@
+export const COLUMNS_FIELDS = [
+    { name: 'Categoria', textDirection: 'left' },
+    { name: 'Tipo', textDirection: 'left' },
+    { name: 'Descrição', textDirection: 'left' },
+    { name: 'Valor', textDirection: 'left' },
+    { name: 'Data Inicial', textDirection: 'left' },
+    { name: 'Data Final', textDirection: 'left' },
+    { name: 'Tipo Conta', textDirection: 'left' },
+    { name: 'Tipo Transação', textDirection: 'left' },
+    { name: 'Dia Referência', textDirection: 'left' },
+    { name: 'Ações', textDirection: 'center' },
+]
